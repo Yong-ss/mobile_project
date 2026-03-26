731cef90171d28cb13594e59b7c0780e8917a56a
Yss <sungshen88@gmail.com>
2026-03-26 23:15:57 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart'; // 加上这个
 import 'package:supabase_flutter/supabase_flutter.dart'; // 加上这个
 import 'utils/supabase_config.dart'; // 导入配置类
 import 'screens/auth/login_screen.dart';
+import 'screens/core/home_screen.dart';
 
 void main() async {
   // 1. 确保 Flutter 绑定初始化（异步 main 必须加这一行）
@@ -16,24 +17,26 @@ void main() async {
     url: SupabaseConfig.url,
     anonKey: SupabaseConfig.anonKey,
   );
+  final session = Supabase.instance.client.auth.currentSession;
 
   // 4. 运行 App
-  runApp(const PrisconApp());
+  runApp(PrisconApp(initialSession: session));
 }
 
 class PrisconApp extends StatelessWidget {
-  const PrisconApp({super.key});
+  final Session? initialSession;
+  const PrisconApp({super.key, this.initialSession});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Priscon',
-      debugShowCheckedModeBanner: false,// hide debug function
+      debugShowCheckedModeBanner: false, // hide debug function
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,// Use modern M3 UI
+        useMaterial3: true, // Use modern M3 UI
       ),
-      home: const LoginScreen(),
+      home: initialSession != null ? const HomeScreen() : const LoginScreen(),
     );
   }
 }
